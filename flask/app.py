37fa1a0480eac204cc71092d7e9609cf71168b82
Chitsanupong <first30952@gmail.com>
2022-02-11 22:43:09 +0700

    feat: add dynamic route

--- flask/app.py
+++ flask/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index(): 
     return '<h1>Hello World!</h1>'
 
+@app.route('/about')
+def about(): 
+    return '<h1>About me</h1>'
+
+@app.route('/user/<username>')
+def user(username):
+    return '<b>User: {} </b>'.format(username)
+
 if __name__ == '__main__':
     app.run()
